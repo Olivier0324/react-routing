f8205022f96c8f99b6d225424883b4221dc07e0a
Olivier0324 <techies.dev24@gmail.com>
2025-09-06 12:57:42 +0200

    I have created pages folder and components folder

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About me</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
