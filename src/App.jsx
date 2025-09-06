77abd7954fd7b0e828fced705b923523b6435e93
Olivier0324 <techies.dev24@gmail.com>
2025-09-06 14:12:58 +0200

    Added styles in the page and added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import { Route, Routes } from 'react-router-dom'
+import Home from '../src/pages/Home'
+import Contact from '../src/pages/Contact'
+import About from '../src/pages/About'
+import PageNotFound from './pages/PageNotFound'
+import Navbar from './components/Navbar'
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={ <Home/>} />
+        <Route path='/about' element={ <About/>} />
+        <Route path='/contact' element={ <Contact/>} />
+        <Route path='*' element={ <PageNotFound/>} />
+      </Routes>
+    </div>
   )
 }
 
